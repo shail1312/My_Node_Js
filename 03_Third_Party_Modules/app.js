71104420e5f2b2a65be15d5c7280a3fe6c601e6f
Shailesh Pandey <Shailesh Pandey>
2017-05-03 12:39:29 -0700

    Third party library

---
+++ 03_Third_Party_Modules/app.js
@@ -0,0 +1,8 @@
+const _ = require('lodash');
+
+/*
+console.log(_.isString(true));
+console.log(_.isString('Shailesh'));
+*/
+var filteredArray = _.uniq(['Shailesh',1,'Shailesh',1,2,3,4]);
+console.log(filteredArray);
