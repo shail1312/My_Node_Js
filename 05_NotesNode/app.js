8b67c37142320eaab72616958a71e7a4f72e9179
shail121 <shail1312@hotmail.com>
2017-05-06 12:36:03 +0530

    Listing notes

--- 05_NotesNode/app.js
+++ 05_NotesNode/app.js
@@ -1,5 +1,3 @@
-console.log('this is app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,8 +6,7 @@ const notes = require('./notes.js');
 
 var argv = yargs.argv;
 var command = process.argv[2];
-console.log('Command: ',command);
-console.log('Yargs : ',yargs.argv);
+
 if(command == 'add'){
   //node app.js add --title=Secret --body="This is my secret"
   var note = notes.addNote(argv.title,argv.body);
@@ -20,7 +17,9 @@ if(command == 'add'){
     console.log('Note Already stored');
   }
 } else if(command == 'list'){
-  notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if (command == 'read') {
   var note = notes.getNote(argv.title);
   if(note){
