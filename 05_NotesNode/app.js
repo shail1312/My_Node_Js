06a200f588140c4a268a0b23d28f8a0d8d1ba39c
shail121 <shail1312@hotmail.com>
2017-05-06 12:58:44 +0530

    Parameter help

--- 05_NotesNode/app.js
+++ 05_NotesNode/app.js
@@ -4,7 +4,32 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title  of note',
+  demand: true,
+  alias: 't'
+};
+
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+var argv = yargs
+  .command('add','Add a new note',{
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list','List of notes')
+  .command('read','Read a note',{
+    title: titleOptions
+  })
+  .command('remove','Remove a note',{
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = process.argv[2];
 
 if(command == 'add'){
@@ -21,17 +46,17 @@ if(command == 'add'){
     console.log(`Printing ${allNotes.length} note(s).`);
     allNotes.forEach((note) => notes.logNote(note));
 } else if (command == 'read') {
-  var note = notes.getNote(argv.title);
-  if(note){
-    console.log('Note found');
-    notes.logNote(note);
-  } else {
-    console.log('Note not found');
-  }
+    var note = notes.getNote(argv.title);
+    if(note){
+      console.log('Note found');
+      notes.logNote(note);
+    } else {
+      console.log('Note not found');
+    }
 } else if (command == 'remove') {
-  var noteRemoved = notes.removeNote(argv.title);
-  var message = noteRemoved ? 'Note was removed' : 'Note not found';
-  console.log(message);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
 } else {
-  console.log('Command not reconized');
+    console.log('Command not reconized');
 }
