a7a6fd935ec587b9355c73adda3b71d5b8fbbd92
shail121 <shail1312@hotmail.com>
2017-05-12 18:26:25 +0530

    Location Lat and lng

--- 06_weather-app/app.js
+++ 06_weather-app/app.js
@@ -4,5 +4,8 @@ request({
   url:'https://maps.googleapis.com/maps/api/geocode/json?address=%20fafadih%20raipur',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
+  //console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
