75c4649ca9ff7f0229844bd5c3ac864af2ede108
shail121 <shail1312@hotmail.com>
2017-05-17 20:35:59 +0530

    Call form weather api

--- 06_weather-app/app.js
+++ 06_weather-app/app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 
@@ -21,3 +22,22 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     console.log(JSON.stringify(results, undefined, 2));
   }
 });
+
+var getTemperature = () => {
+  request({
+    url:`https://api.darksky.net/forecast/de19258c7e48a67d69ac5d955b103f1d/21.2634837,81.61720830000002`,
+    json: true
+  }, (error, response, body) => {
+    if(error){
+      console.log('Unable to connect weather api.');
+    }
+    else if (response.statusCode === 400) {
+      console.log('Unable to fetch weather.');
+    } else if(response.statusCode === 200){
+      console.log('temperature'+body.currently.temperature);
+    }
+  });
+};
+getTemperature();
+
+//de19258c7e48a67d69ac5d955b103f1d
