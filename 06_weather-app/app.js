db4771483bd4cc1a69fff81a3752ddfd38461ea3
shail121 <shail1312@hotmail.com>
2017-05-17 19:46:20 +0530

    Abstracting callbacks

--- 06_weather-app/app.js
+++ 06_weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 .options({
@@ -14,21 +14,10 @@ const argv = yargs
 .alias('help','h')
 .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-//console.log(encodedAddress);
-
-request({
-  url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  //console.log(JSON.stringify(response, undefined, 2));
-  if(error) {
-    console.log('Unable to connect to Google server.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find the address.');
-  } else if(body.status === 'OK'){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
