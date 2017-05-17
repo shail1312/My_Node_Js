e0226e320d1448da05db8fa77c6cd5814d0709ea
shail121 <shail1312@hotmail.com>
2017-05-17 19:13:12 +0530

    Handle error of finding location

--- 06_weather-app/app.js
+++ 06_weather-app/app.js
@@ -15,15 +15,20 @@ const argv = yargs
 .argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
-
-console.log(encodedAddress);
+//console.log(encodedAddress);
 
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}',
+  url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   //console.log(JSON.stringify(response, undefined, 2));
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  if(error) {
+    console.log('Unable to connect to Google server.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find the address.');
+  } else if(body.status === 'OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  }
 });
