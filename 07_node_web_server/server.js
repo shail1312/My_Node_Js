3670a53dd890b2ebdc586d742b496b66a27bd456
shail121 <shail1312@hotmail.com>
2017-05-23 22:13:51 +0530

    Handlerbar templating

--- 07_node_web_server/server.js
+++ 07_node_web_server/server.js
@@ -3,14 +3,23 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('scremeIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   //res.send('<h1>hello express!</h1>');
   res.render('index.hbs',{
-    name: 'Shailesh',
+    pageTitle: 'Welcome to the express',
     likes: [
       'Programming',
       'Gamming'
@@ -20,8 +29,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req,res) => {
   res.render('about.hbs',{
-    pageTitle: 'About page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About page'
   });
 });
 
