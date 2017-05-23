dc706f5fa7a2ed2c1d2d40baac9ef7ae89cf97d2
shail121 <shail1312@hotmail.com>
2017-05-23 20:21:43 +0530

    Add handlebars for web templating

--- 07_node_web_server/server.js
+++ 07_node_web_server/server.js
@@ -1,10 +1,15 @@
-const experss = require('express');
+const express = require('express');
+const hbs = require('hbs');
 
-var app = experss();
+var app = express();
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   //res.send('<h1>hello express!</h1>');
-  res.send({
+  res.render('index.hbs',{
     name: 'Shailesh',
     likes: [
       'Programming',
@@ -14,7 +19,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req,res) => {
-  res.send('About page');
+  res.render('about.hbs',{
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is on port 3000');
+});
