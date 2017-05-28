428ff5f37e9351de27c44315c31b3fad3bf2aef2
shail121 <shail1312@hotmail.com>
2017-05-28 15:27:55 +0530

    Using expect

--- 08_node_test/utils/utils.test.js
+++ 08_node_test/utils/utils.test.js
@@ -1,17 +1,28 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(11, 22);
 
-  if(res !== 33) {
-    throw new Error(`Expected 33 but value is ${res}.`);
-  }
+  expect(res).toBe(33).toBeA('number');
 });
 
 it('should square number', () => {
   var res = utils.square(3);
 
-  if(res !== 9) {
-    throw new Error(`Expected 9 but value is ${res}.`);
-  }
+  expect(res).toBe(9).toBeA('number');
+});
+
+//should verify first and lastName and set
+// assert it include firstName and lastName with proper values
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'India', age: 23};
+  var res = utils.setName(user, 'Shailesh Pandey');
+
+  expect(res).toInclude({
+    firstName: 'Shailesh',
+    lastName: 'Pandey'
+  });
+
 });
