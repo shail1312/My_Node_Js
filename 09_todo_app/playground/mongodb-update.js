468eb5f4c5bc235a8af02dc07cd26b95425a5153
shail121 <shail1312@hotmail.com>
2017-05-31 00:10:43 +0530

    Update on mongodb

---
+++ 09_todo_app/playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id:new ObjectID("592bcfcdca4c8621aa2e9d20")
+  // }, {
+  //   $set: {
+  //     completed:true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id:new ObjectID("592bd189bb25af2469fa2dae")
+  }, {
+    $set: {
+      name: 'Dev'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+
+  //db.close();
+});
