e9bd1d89ac26a44b7baa2cc90714b6bc1492de86
shail121 <shail1312@hotmail.com>
2017-05-30 22:58:27 +0530

    Find in mongodb

--- 09_todo_app/playground/mongodb-connect.js
+++ 09_todo_app/playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
   if(err){
@@ -17,16 +21,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name:'Shailesh',
-    age:23,
-    location: 'Raipur'
-  }, (err, result) => {
-    if(err) {
-      return console.log('Unable to insert user', err);
-    }
-    console.log(result.ops);
-  });
+  // db.collection('Users').insertOne({
+  //   name:'Shailesh',
+  //   age:23,
+  //   location: 'Raipur'
+  // }, (err, result) => {
+  //   if(err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
