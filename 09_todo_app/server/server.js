d750a57e450229404186ea051987a887666dc654
shail121 <shail1312@hotmail.com>
2017-06-12 20:52:52 +0530

    Validation on mongose

--- 09_todo_app/server/server.js
+++ 09_todo_app/server/server.js
@@ -5,34 +5,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text:{
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 // var newTodo = Todo({
-//   text: 'Do homework'
+//   text: 'Play Football',
+//   completed: true,
+//   completedAt:50
 // });
-//
+
 // newTodo.save().then((doc) => {
-//   console.log('Save todo', doc);
+//   console.log(JSON.stringify(doc, undefined, 2) );
 // }, (e) => {
 //   console.log('Unable to save todo');
 // });
 
-var newTodo = Todo({
-  text: 'Play Football',
-  completed: true,
-  completedAt:50
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-newTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2) );
+var user = new User({
+  email: 'shail1312@hotmail.com   '
+});
+
+user.save().then((doc)=> {
+  console.log('User saved',doc);
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unable to save the user',e);
 });
