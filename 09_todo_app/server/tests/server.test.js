5339e8e2a6816796cba9dfd565a85f073cbda418
shail121 <shail1312@hotmail.com>
2017-06-14 17:11:26 +0530

    Add test case for GET /todos/:id

--- 09_todo_app/server/tests/server.test.js
+++ 09_todo_app/server/tests/server.test.js
@@ -72,3 +72,31 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+describe('GET /todos/:id',() => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/abc')
+      .expect(404)
+      .end(done);
+  });
+});
